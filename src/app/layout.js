0a5b273eb07ae0646be0c35ff9efd35550c06a90
Omeiza Owuda <owudaomeiza@gmail.com>
2025-10-25 20:49:28 +0200

    og image

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,8 +3,26 @@ import "@/styles/globals.css";
 import Header from "@/component/layout/header";
 
 export const metadata = {
-  title: "Activ Media Intelligence",
-  description: "Uncover Media Insights Across Africa's Digital Landscape",
+	metadataBase: 'https://activ.africa',
+	title: "Activ | Media Intelligence for Africa",
+	description: "Uncover Media Insights Across Africa's Digital Landscape",
+	openGraph: {
+		title: "Activ | Media Intelligence for Africa",
+		description: "Uncover Media Insights Across Africa's Digital Landscape",
+		url: "https://activ.africa",
+		siteName: "Activ Media Intelligence",
+		images: [
+		{ url: "/images/og.png", width: 1200, height: 630, alt: "Activ Media Intelligence" }
+		],
+		locale: "en_US",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Activ | Media Intelligence for Africa",
+		description: "Uncover Media Insights Across Africa's Digital Landscape",
+		images: ["/images/og.png"],
+	},
 };
 
 export default function RootLayout({ children }) {
